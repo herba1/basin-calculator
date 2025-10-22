c883b76afedbb543aef4f7ec463b505540d7cb2d
herbart hernandez <burnedcard77@gmail.com>
2025-10-22 11:35:58 -0700

    before next intl

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,33 +1,63 @@
-'use client'
+"use client";
 
-import { useNav } from "@/context/NavContext"
+import { useNav } from "@/context/NavContext";
 import Image from "next/image";
 import Link from "next/link";
 import cn from "../utils/cn";
 
 const navLeftLinks = [
-    {name:'problem',url:'#problem',subLinks:[]},
-    {name:'solution',url:'/', subLinks:[]},
-    {name:'extra',url:'/', subLinks:[]},
+  { name: "Problem", url: "#problem", subLinks: [] },
+  { name: "Solution", url: "#solution", subLinks: [] },
+  { name: "Extra", url: "", subLinks: [] },
+];
 
-]
+export default function NavigationMenu({ className = "" }) {
+  const nav = useNav();
 
-export default function NavigationMenu({className=""}){
-    const nav = useNav();
-    console.log(nav)
+  return (
+    <nav className={`${className} nav__container py-3 px-3`}>
+      <ul className="nav__list flex nav__left">
+        <Image
+          src="/cwi_logo.svg"
+          width={100}
+          height={100}
+          alt="none"
+          className="w-19 mr-3"
+        ></Image>
+        {navLeftLinks.map((e) => {
+          return (
+            <NavLink key={e.name} href={e.url}>
+              {e.name}
+            </NavLink>
+          );
+        })}
+      </ul>
+      <ul className="nav__list flex nav__right">
+        <button></button>
 
-    return (<nav className={`${className} nav__container py-3 px-3`}>
-        <ul className="nav__list nav__left">
-            <Image src="/cwi_logo.svg" width={100} height={100} alt="none" className="w-19"></Image>
-        </ul>
-        <ul className="nav__list nav__right">
-        </ul>
-    </nav>)
-
+      </ul>
+    </nav>
+  );
 }
 
-function NavLink({url="/",name="name", className=""}:{url:string, name:string, className?:string}){
-
-    return <Link className={cn('',className)} href={url}>{name}</Link>
-
+function NavLink({
+  href = "/",
+  children,
+  className = "",
+}: {
+  href: string;
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <Link
+      className={cn(
+        " px-6 py-3 transition-colors duration-150 ease focus:text-highlight font-bold hover:text-highlight",
+        className
+      )}
+      href={href}
+    >
+      {children}
+    </Link>
+  );
 }
